8460c984a528c0e982a672c2a652f72166f5d90b
Sather <satherbosco@hotmail.com>
2022-01-24 11:02:32 -0300

    btn-top att

--- js/show-btn-top.js
+++ js/show-btn-top.js
@@ -9,8 +9,3 @@ function scrollFunction() {
         mybutton.style.display = "none";
     }
 }
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
